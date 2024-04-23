30a33375263a0d10338aad6e054eadba6dddac6d
Nakk-i <nastyabond90@gmail.com>
2024-04-23 15:59:38 +0300

    hw1 add

--- hw_1.js
+++ hw_1.js
@@ -1,50 +1,40 @@
-const fsPromises  = require('node:fs/promises')
+const fs  = require('node:fs/promises')
 const path = require("node:path");
+
 
 async  function createDirect(){
     try {
+        const basePath = path.join(process.cwd(), 'baseFolder');
+        await fs.mkdir(basePath, {recursive:true});
 
-        await fsPromises.mkdir(path.join(__dirname, 'baseFolder', 'one'), {recursive:true});
-        await fsPromises.mkdir(path.join(__dirname, 'baseFolder',  'two'), {recursive:true});
-        await fsPromises.mkdir(path.join(__dirname, 'baseFolder', 'three'), {recursive:true});
-        await fsPromises.mkdir(path.join(__dirname, 'baseFolder',  'four'), {recursive:true});
-        await fsPromises.mkdir(path.join(__dirname, 'baseFolder',  'five'), {recursive:true});
+        const folderNames =['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
+        const fileNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'];
 
-        //one
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'one', 'file_1.txt'), 'First five!');
-        await fsPromises.copyFile(path.join(__dirname, 'baseFolder', 'one', 'file_1.txt'), path.join(__dirname, 'baseFolder', 'one', 'file_2.txt'));
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'one', 'file_3.txt'), 'First five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'one', 'file_4.txt'), 'First five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'one', 'file_5.txt'), 'First five!');
+        for (const folderName of folderNames) {
+            const folderPath = path.join(basePath, folderName);
+            await fs.mkdir(folderPath, {recursive:true});
 
-        //two
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'two', 'file_6.txt'), 'Second five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'two', 'file_7.txt'), 'Second five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'two', 'file_8.txt'), 'Second five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'two', 'file_9.txt'), 'Second five!');
-        await fsPromises.copyFile(path.join(__dirname, 'baseFolder', 'two', 'file_6.txt'), path.join(__dirname, 'baseFolder', 'two', 'file_10.txt'));
+            for (const fileName of fileNames) {
+                await fs.writeFile(path.join(folderPath, fileName), 'Hello World!')
+            }
+        }
 
-        //three
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'three', 'file_11.txt'), 'Third five!')
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'three', 'file_12.txt'), 'Third five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'three', 'file_13.txt'), 'Third five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'three', 'file_14.txt'), 'Third five!');
-        await fsPromises.copyFile(path.join(__dirname, 'baseFolder', 'three', 'file_11.txt'), path.join(__dirname, 'baseFolder', 'three', 'file_15.txt'));
+        const result = await fs.readdir(basePath);
+        console.log(result)
+        for (const folderName of result) {
+            const folderPath = path.join(basePath, folderName)
+            const stat = await fs.stat(path.join(folderPath));
+            console.log('STAT : isDirectory: ', stat.isDirectory())
 
-        //four
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'four', 'file_16.txt'), 'Fourth five!')
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'four', 'file_17.txt'), 'Fourth five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'four', 'file_18.txt'), 'Fourth five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'four', 'file_19.txt'), 'Fourth five!');
-        await fsPromises.copyFile(path.join(__dirname, 'baseFolder', 'four', 'file_16.txt'), path.join(__dirname, 'baseFolder', 'four', 'file_20.txt'));
+            const result = await fs.readdir(folderPath);
+            for (const item of result) {
+                const itemPath = path.join(folderPath, item)
+                const stat = await fs.stat(path.join(itemPath));
+                console.log('STAT : isDirectory: ', stat.isDirectory()
+                )
+            }
 
-        //five
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'five', 'file_21.txt'), 'Fifth five!')
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'five', 'file_22.txt'), 'Fifth five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'five', 'file_23.txt'), 'Fifth five!');
-        await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'five', 'file_24.txt'), 'Fifth five!');
-        await fsPromises.copyFile(path.join(__dirname, 'baseFolder', 'five', 'file_21.txt'), path.join(__dirname, 'baseFolder', 'five', 'file_25.txt'));
-
+        }
     } catch (e){
         console.error(e)
     }
